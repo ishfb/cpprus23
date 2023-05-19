d79c22830d70c1d441cb3d8f6b9eaa359facdb6b
Ilia <shishkov.ilia@gmail.com>
2023-05-19 20:48:08 +0300

    Benchmark and data for sets comparison

--- complexity_benchmark/sets_compare.cpp
+++ complexity_benchmark/sets_compare.cpp
@@ -11,42 +11,42 @@
 #include <vector>
 using namespace std;
 
-std::vector<int> TestData(size_t size) {
+template <typename RandomEngine>
+std::vector<int> TestData(size_t size, RandomEngine& re) {
+  std::uniform_int_distribution<int> gen(1, 10'000'000);
   vector<int> r(size);
   for (int& x : r) {
-    x = rand();
+    x = gen(re);
   }
   return r;
 }
 
-template <typename Set> void SetFind(benchmark::State& state) {
-  auto values = TestData(state.range(0));
+template <typename Set>
+void Find(benchmark::State& state) {
+  std::default_random_engine re(20230518);
+  const auto values = TestData(state.range(0), re);
   Set sud(values.begin(), values.end());
 
-  std::default_random_engine re(20230518);
-  std::uniform_int_distribution<int> take(0, 100);
-  std::uniform_int_distribution<int> gen_random(-1000000, 1000000);
-
+  std::uniform_int_distribution<size_t> take_absent(1, 100);
   for (auto _ : state) {
-    for (int x : values) {
-      if (take(re) < 50) {
-        x = gen_random(re);
-      }
-      auto it = sud.find(x);
-      benchmark::DoNotOptimize(it);
+    int x = values.front();
+    if (take_absent(re) > 50) {
+      x = -x;
     }
+    auto it = sud.find(x);
+    benchmark::DoNotOptimize(it);
   }
   state.SetComplexityN(values.size());
 }
 
 // Register the function as a benchmark
-BENCHMARK(SetFind<std::unordered_set<int>>)
+BENCHMARK(Find<std::unordered_set<int>>)
     ->RangeMultiplier(2)
     ->Range(1, 1 << 17)
-    ->Complexity(benchmark::oN);
-BENCHMARK(SetFind<boost::container::flat_set<int>>)
+    ->Complexity(benchmark::o1);
+BENCHMARK(Find<boost::container::flat_set<int>>)
     ->RangeMultiplier(2)
     ->Range(1, 1 << 17)
-    ->Complexity(benchmark::oNLogN);
+    ->Complexity(benchmark::oLogN);
 
 BENCHMARK_MAIN();
